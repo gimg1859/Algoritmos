76e563ed9575f5645cc3f11064a22dcc1b6d4f8a
kavanegasm <kavanegasm@unal.edu.co>
2018-03-04 11:56:24 -0500

    Update fibonaci.java

--- Taller1/fibonachi/java/fibonaci.java
+++ Taller1/fibonachi/java/fibonaci.java
@@ -7,44 +7,52 @@ public class Fibonacci
  {
     public static Scanner scann= new Scanner(System.in);
     
-    public static byte fByte(int n){
+    public static byte fByte(int n)
+    {
         byte first=0;
         byte second=1;
         byte returnsorce=0;
-        for (int i = 0; i < n-1; i++) {
+        for (int i = 0; i < n-1; i++) 
+        {
             returnsorce=(byte) (first+second);
             second=first;
             first=returnsorce;
         }
         return returnsorce;
     }
-    public static short fShort(int n){
+    public static short fShort(int n)
+    {
         short first=0;
         short second=1;
         short returnsorce=0;
-        for (int i = 0; i < n-1; i++) {
+        for (int i = 0; i < n-1; i++)
+        {
             returnsorce=(short) (first+second);
             second=first;
             first=returnsorce;
         }
         return returnsorce;
     }
-    public static int fInt(int n){
+    public static int fInt(int n)
+    {
         int first=0;
         int second=1;
         int returnsorce=0;
-        for (int i = 0; i < n-1; i++) {
+        for (int i = 0; i < n-1; i++)
+        {
             returnsorce=(int) (first+second);
             second=first;
             first=returnsorce;
         }
         return returnsorce;
     }
-    public static long fLong(int n){
+    public static long fLong(int n)
+    {
         long first=0;
         long second=1;
         long returnsorce=0;
-        for (int i = 0; i < n-1; i++) {
+        for (int i = 0; i < n-1; i++) 
+        {
             returnsorce=(long) (first+second);
             second=first;
             first=returnsorce;
@@ -52,30 +60,36 @@ public class Fibonacci
         return returnsorce;
     }
     
-    public static boolean salir(){
+    public static boolean salir()
+    {
         int opt=-1;
         do {            
             System.out.println("Desea salir\n\t-1 Si\n\t-2 No\n");
             opt=scann.nextInt();
-            if (opt<1 || opt>2) {
+            if (opt<1 || opt>2)
+            {
                 System.out.println("-------------------------------------\n");
-                System.out.println("Por favor indicar una opt correcta\n");
+                System.out.println("Por favor indicar una opcion correcta\n");
             }
-        } while (opt<1 || opt>2);
-        if(opt==1){
+        }
+     while (opt<1 || opt>2);
+        if(opt==1)
+        {
             return true;
         }else{
             return false;
         }
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         int opt=-1;
         boolean salir=false;
         while (!salir) {            
             do {            
                 System.out.println("Indique con que tipo de dato desea trabajar\n\t-1 Byte\n\t-2 Short\n\t-3 Int\n\t-4 Long\n");
                 opt=scann.nextInt();
-                if(opt<1 || opt>4){
+                if(opt<1 || opt>4)
+                {
                     System.out.println("-------------------------------------\n");
                     System.out.println("Por favor indicar una opcion correcta\n");
                 }
@@ -83,7 +97,8 @@ public class Fibonacci
             int n=3;
             switch(opt){
                     case 1:
-                        while(fByte(n)>0){
+                        while(fByte(n)>0)
+                        {
                             n++;
                         }
                         System.out.println("Overflow en byte en n="+n);
@@ -92,7 +107,8 @@ public class Fibonacci
                     break;
 
                     case 2:
-                        while(fShort(n)>0){
+                        while(fShort(n)>0)
+                        {
                             n++;
                         }
                         System.out.println("Hizo Overflow en n="+n);
@@ -100,7 +116,8 @@ public class Fibonacci
                     break;
 
                     case 3:
-                        while(fInt(n)>0){
+                        while(fInt(n)>0)
+                        {
                             n++;
                         }
                        System.out.println("Hizo Overflow en n="+n);
@@ -108,7 +125,8 @@ public class Fibonacci
                     break;
 
                     case 4:
-                        while(fLong(n)>0){
+                        while(fLong(n)>0)
+                        {
                             n++;
                         }
                         System.out.println("Hizo Overflow en n="+n);
